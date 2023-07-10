d47148f709ebca7731875cc9cf4fa846645bb56c
Monineath Sar <monineathsar@Monineaths-MacBook-Air.local>
2023-07-09 22:32:32 -0700

    adding & saving new expense data

--- src/App.js
+++ src/App.js
@@ -30,10 +30,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ]
+
+  const addExpenseHandler = expense => {
+    console.log(expense)
+  }
 // below is example of JSX syntax
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
